42132f7d7b7f03d6aa247ae8748d4c3a1fa3a55e
Conner185338 <47399959+Conner185338@users.noreply.github.com>
2019-05-14 10:03:22 -0400

    Files added

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+
+namespace rocketRiotv2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        
+        System.Windows.Threading.DispatcherTimer gameTimer = new System.Windows.Threading.DispatcherTimer();
+        Zapper zapper;
+        string[] highScores = new string[5];
+        string[] highScoreData;
+        Button btnStartGame = new Button();
+        Button btnHighScores = new Button();
+
+        public MainWindow()
+        {
+            Rectangle sprite = new Rectangle();
+            Rectangle coins = new Rectangle();
+            Rectangle background = new Rectangle();
+            Rectangle[] rectangles = new Rectangle[10];
+            Random rand = new Random();
+            ImageBrush spritefill;
+            BitmapImage bitmapImage;
+            gameTimer.Interval = new TimeSpan(0, 0, 0, 0, 20);
+            gameTimer.Tick += GameTimer_Tick;
+            InitializeComponent();
+            background.Height = 600;
+            background.Width = 800;
+            bitmapImage = new BitmapImage(new Uri("background.png", UriKind.Relative));
+            spritefill = new ImageBrush(bitmapImage);
+            background.Fill = spritefill;
+            canvas.Children.Add(background);
+
+            
+            btnStartGame.Click += btnStartGame_Click;
+            btnStartGame.Content = "Start Game";
+            btnStartGame.FontSize = 50;
+            btnStartGame.Background = Brushes.White;
+            Canvas.SetLeft(btnStartGame, 265);
+            Canvas.SetTop(btnStartGame, 50);
+            btnStartGame.BorderThickness = new Thickness(1);
+            btnStartGame.BorderBrush = Brushes.Black;
+            canvas.Children.Add(btnStartGame);
+
+            btnHighScores.Click += btnHighScores_Click;
+            btnHighScores.Content = "High Scores";
+            btnHighScores.FontSize = 40;
+            btnHighScores.Background = Brushes.White;
+            Canvas.SetLeft(btnHighScores, 285);
+            Canvas.SetTop(btnHighScores, 125);
+            btnHighScores.BorderThickness = new Thickness(1);
+            btnHighScores.BorderBrush = Brushes.Black;
+            canvas.Children.Add(btnHighScores);
+
+            System.IO.StreamReader readHighScores = new System.IO.StreamReader("HighScores.txt");
+            for (int i = 0; i < 5; i++)
+            {
+                highScores[i] = readHighScores.ReadLine();
+            }
+            readHighScores.Close();
+        }
+
+        private void btnStartGame_Click(object sender, RoutedEventArgs e)
+        {
+
+            gameTimer.Start();
+
+            canvas.Children.Remove(btnStartGame);
+            canvas.Children.Remove(btnHighScores);
+
+            int temp = 1;
+            zapper = new Zapper(temp, canvas);
+            zapper.generate();
+
+        }
+
+        private void GameTimer_Tick(object sender, EventArgs e)
+        {
+            zapper.animate();
+            zapper.generate();
+        }
+
+        /// <summary>
+        /// Who wrote it
+        /// purpose of the method
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnHighScores_Click(object sender, RoutedEventArgs e)
+        {
+            Label lblHighScores = new Label();
+            canvas.Children.Remove(btnStartGame);
+            canvas.Children.Remove(btnHighScores);
+
+            
+            lblHighScores.FontSize = 15;
+            //lblHighScores.Background.Opacity = .5;
+            //lblHighScores.Background = Brushes.White;
+            lblHighScores.Background = new SolidColorBrush(Color.FromArgb(125, 255, 255, 255));
+            lblHighScores.Foreground = Brushes.Black;
+
+            Canvas.SetLeft(lblHighScores, 350 - lblHighScores.ActualWidth);
+            Canvas.SetTop(lblHighScores, 120 - lblHighScores.ActualHeight);
+            canvas.Children.Add(lblHighScores);
+            
+
+
+            lblHighScores.Content = "";
+            for (int i = 0; i < 5; i++)
+            {
+                highScoreData = highScores[i].Split(new char[] { ',' });
+                lblHighScores.Content += i + 1 + ". " + highScoreData[0] + " " + highScoreData[1] + Environment.NewLine;
+            }
+        }
+    }
+}
+
---
+++ Zapper.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Drawing;
+
+namespace rocketRiotv2
+{
+    class Zapper
+    {
+        //global variables
+        public int quadrant;
+        int spriteNum;
+        int counter2 = 0;
+        BitmapImage bitmapImage;
+        ImageBrush ZapperFill;
+        public Rectangle zapper = new Rectangle();
+        Random h = new Random();
+        Canvas canvas = new Canvas();
+        int counter = 0;
+        string spriteName = "Vertical Sprite 1.png";
+        //methods
+        public Zapper(int q, Canvas c)
+        {
+            quadrant = q;
+            canvas = c;
+            bitmapImage = new BitmapImage(new Uri(spriteName, UriKind.Relative));
+            ZapperFill = new ImageBrush(bitmapImage);
+            zapper.Fill = ZapperFill;
+            zapper.Height = 50;
+            zapper.Width = 50;
+            canvas.Children.Add(zapper);
+            Canvas.SetTop(zapper, 300);
+            Canvas.SetLeft(zapper, 300);
+        }
+        public void generate()
+        {
+            canvas.Children.Remove(zapper);
+            bitmapImage = new BitmapImage(new Uri(spriteName, UriKind.Relative));
+            ZapperFill = new ImageBrush(bitmapImage);
+            zapper.Fill = ZapperFill;
+            zapper.Height = 50;
+            zapper.Width = 50;
+            Canvas.SetTop(zapper, 300);
+            Canvas.SetLeft(zapper, 300);
+            RotateTransform rotate = new RotateTransform(counter2 * 50);
+            zapper.RenderTransformOrigin = new Point(0.5, 0.5);
+            zapper.RenderTransform = rotate;
+            canvas.Children.Add(zapper);
+        }
+        public void animate()
+        {
+            counter++;
+            counter2++;
+            //change sprite every 10 counts
+            if (counter < 3)
+            {
+                spriteNum = 1;
+            }
+            else if (counter > 3)
+            {
+                spriteNum = 2;
+
+                if (counter == 6)
+                {
+                    counter = 0;
+                }
+            }
+            spriteName = "Vertical Sprite " + spriteNum + ".png";
+        }
+    }
+}
